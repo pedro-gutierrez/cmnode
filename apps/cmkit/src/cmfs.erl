7d38fe2b3219e252165937a84d9a7e3535961370
Pedro Gutiérrez <pedrogutierrez@mac.com>
2018-07-16 15:16:23 +0200

    fixed file streaming not found scenario

--- apps/cmkit/src/cmfs.erl
+++ apps/cmkit/src/cmfs.erl
@@ -1,8 +1,11 @@
 -module(cmfs).
 -export([stream/1]).
 
-stream(#{ path := Filename,
-          context := #{ callback := {M, F} } = Context }=Spec) ->
+stream(#{ stream := Stream,
+          path := Filename,
+          context := #{ data := Data,
+                        callback := {M, F} } = Context }=Spec) ->
+    cmkit:log({cmfs, stream, Spec}),
     Probes = lists:map(fun(N) -> 
                                case rpc:call(N, cmkit, file_info, [Filename]) of 
                                    {ok, Info} -> Info#{ node => N,
@@ -18,14 +21,15 @@ stream(#{ path := Filename,
                        end, [node()|nodes()]),
     
     case aggregate(Probes) of 
-        not_found -> ok;
+        not_found -> 
+            cmkit:stream_error(Stream, 404, Data, {M, F});
         {ok, #{ node := N}} -> 
             Spec2 = Spec#{ context => Context#{ callback => {node(), M, F}}},
             rpc:call(N, cmkit, stream_file, [Spec2])
     end.
 
 aggregate([]) -> not_found;
-aggregate([not_found|Rem]) -> aggregate(Rem);
+aggregate([#{ status := not_found }|Rem]) -> aggregate(Rem);
 aggregate([#{ status := error }|Rem]) -> aggregate(Rem);
 aggregate([#{ status := found }=Loc|_]) -> {ok, Loc}.
 
