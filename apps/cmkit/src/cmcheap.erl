b4beaabc82d113405fde9fb0bf61b3752a8962ce
Pedro Gutiérrez <pedrogutierrez@mac.com>
2019-01-31 10:43:48 +0100

    better parsing of namecheap responses

--- apps/cmkit/src/cmcheap.erl
+++ apps/cmkit/src/cmcheap.erl
@@ -19,14 +19,14 @@ set_hosts(Hosts, #{ sld := Sld,
 
 
 hosts(Config) ->
-    cmd(get, <<"namecheap.domains.dns.getHosts">>, #{}, Config).
+    cmd(get, <<"namecheap.domains.dns.getHosts">>, 'DomainDNSGetHostsResult', #{}, Config).
 
-cmd(Method, Command, _Extra, #{ sld := Sld,
+cmd(Method, Command, Resp, _Extra, #{ sld := Sld,
                                 tld := Tld,
                                 user := User,
                                 key := ApiKey }) -> 
     
-    parse(cmhttp:do(#{ method => Method,
+    extract(cmhttp:do(#{ method => Method,
                  url => <<"https://api.namecheap.com/xml.response">>,
                  query => #{ 'ApiUser' => User,
                              'UserName' => User,
@@ -34,15 +34,57 @@ cmd(Method, Command, _Extra, #{ sld := Sld,
                              'Command' => Command,
                              'ClientIp' => client_ip(),
                              'SLD' => Sld,
-                             'TLD' => Tld }})).
+                             'TLD' => Tld }}), Resp).
 
 client_ip() ->
     cmkit:to_bin(cmkit:ip_str(cmkit:ipv4())).
 
 
-parse({ok, #{ status := 200,
-              body := Body }}) ->
-    {ok, Body};
+extract({ok, #{ status := 200,
+              body := {'ApiResponse', Attrs, Children}}}, RespName) ->
 
-parse({ok, #{ status := Other }}) ->
+    case lists:keyfind('Status', 1, Attrs) of 
+        false ->
+            unknown_err();
+        {_, "ERROR"} ->
+            extract_error(Children);
+        {_, "OK"} ->
+            extract_response(Children, RespName)
+    end;
+    
+
+extract({ok, #{ status := Other }}, _) when Other =/= 200 ->
     {error, Other}.
+
+extract_error(Resp) ->
+    case lists:keyfind('Errors', 1, Resp) of 
+        false ->
+            unknown_err();
+        {_, _, Errors} ->
+            case lists:keyfind('Error', 1, Errors) of 
+                false ->
+                    unknown_err();
+                {_, _, [E]} ->
+                    err(E)
+            end
+    end.
+
+extract_response(Resp, RespName) ->
+    case lists:keyfind('CommandResponse', 1, Resp) of 
+        false ->
+            unknown_err();
+        {_, _, Resp} ->
+            case lists:keyfind(RespName, 1, Resp) of 
+                false ->
+                    unknown_err();
+                {_, _, Payload} ->
+                    {ok, Payload}
+            end              
+    end.
+
+unknown_err() ->
+    err(unknown).
+
+err(E) ->
+    {error, E}.
+    
